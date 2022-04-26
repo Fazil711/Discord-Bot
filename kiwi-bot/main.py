42f499fa2614b7af85991e3540ab7c9acc3b78e4
Fazil Ahmed Shaikh <44942652+Fazil711@users.noreply.github.com>
2022-04-26 23:23:07 +0530

    Update main.py

--- kiwi-bot/main.py
+++ kiwi-bot/main.py
@@ -1,10 +1,11 @@
 from discord.ext import commands
 import os
+#from keep_alive import keep_alive
 
 bot = commands.Bot(command_prefix='$')
 bot.remove_command('help')
 
-cogs = ['cogs.mods'] 
+cogs = ['cogs.mods', 'cogs.GoogleClass'] 
 
 for cogs in cogs:
   try:
@@ -12,4 +13,5 @@ for cogs in cogs:
   except Exception as e:
     raise e
 
-bot.run('TOKEN')
+
+bot.run('Token')
